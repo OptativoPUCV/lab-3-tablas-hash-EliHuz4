c858309154361cefec5c6f77e83a7be5f53b7878
Your Name <you@example.com>
2025-04-18 17:08:24 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -49,7 +49,7 @@ void insertMap(HashMap * map, char * key, void * value) {
         map->size++;
     }
     else{
-        for(int i = Indice; map->buckets[i] != NULL; i++)
+        for(int i = (Indice + 1) % map->capacity; i != Indice; i = (i + 1) % map->capacity)
         {
             if(map->buckets[i] == NULL)
             {
