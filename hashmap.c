dc034c3503318607d08a5fcc6b9e54fd10f8cce0
Your Name <you@example.com>
2025-04-18 17:36:33 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -142,7 +142,7 @@ Pair * nextMap(HashMap * map) {
         if(map->buckets[i] != NULL && map->buckets[i]->key != NULL)
         {
             map->current = i;
-            return map->buckets[i];
+            return map->buckets[i]->value;
         }
     }
     return NULL;
